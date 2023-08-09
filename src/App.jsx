99b280b719579f99f0c0211a161fdff1058b627d
Shubh Arya <shubh.arya37@gmail.com>
2023-08-09 21:30:52 +0530

    added 2 new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+import "./App.css";
+import Login from "./pages/LoginPage";
+import Register from "./pages/RegisterPage";
 
 function App() {
-
   return (
-    <h1>Hello from richpanel frontend</h1>
-  )
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <ToastContainer />
+    </div>
+  );
 }
 
-export default App
+export default App;
