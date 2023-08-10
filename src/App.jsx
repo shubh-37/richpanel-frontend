8721a633457d8c611517d2c13987bca3eae17e3a
Shubh Arya <shubh.arya37@gmail.com>
2023-08-10 17:40:15 +0530

    added a new route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import "./App.css";
 import Login from "./pages/LoginPage";
 import Register from "./pages/RegisterPage";
+import SubscriptionDetail from "./pages/SubscriptionDetailPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/plans" element={<SubscriptionDetail />} /> 
       </Routes>
       <ToastContainer />
     </div>
