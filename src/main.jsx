a7b75afc0a8e57bd450b03ffd03648f90deb768f
Shubh Arya <shubh.arya37@gmail.com>
2023-08-11 03:01:35 +0530

    added product context

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AuthProvider from "./context/AuthContextProvider.jsx";
+import ProductContextProvider from "./context/ProductContextProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <AuthProvider>
-        <App />
+        <ProductContextProvider>
+          <App />
+        </ProductContextProvider>
       </AuthProvider>
     </Router>
   </React.StrictMode>
