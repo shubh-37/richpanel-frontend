fdfabb63db00e32b67a13aced4e46ffd3a510bbb
Shubh Arya <shubh.arya37@gmail.com>
2023-08-10 17:41:28 +0530

    subscription data

---
+++ src/data.js
@@ -0,0 +1,37 @@
+const app = [
+  {
+    planName: "Mobile",
+    monthlyPrice: 100,
+    yearlyPrice: 1000,
+    videoQuality: "Good",
+    resolution: "480p",
+    devices: ["Phone"],
+  },
+  {
+    planName: "Standard",
+    monthlyPrice: 200,
+    yearlyPrice: 2000,
+    videoQuality: "Good",
+    resolution: "780p",
+    devices: ["Phone", "Tablet"],
+  },
+  {
+    planName: "Premium",
+    monthlyPrice: 500,
+    yearlyPrice: 5000,
+    videoQuality: "Better",
+    resolution: "1080p",
+    devices: ["Phone", "Tablet", "Computer"],
+  },
+  {
+    planName: "Regular",
+    planLabel: "Yearly",
+    monthlyPrice: 700,
+    yearlyPrice: 7000,
+    videoQuality: "Best",
+    resolution: "4K+HDR",
+    devices: ["Phone", "Tablet", "TV", "Computer"],
+  },
+];
+
+export default app;
