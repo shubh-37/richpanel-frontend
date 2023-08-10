748c23645144450dd98fea3e963f71d13efa53f3
Shubh Arya <shubh.arya37@gmail.com>
2023-08-11 03:04:02 +0530

    added a checkout component from stripe documentation

---
+++ src/components/CheckoutForm.jsx
@@ -0,0 +1,39 @@
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+function CheckoutForm() {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const cardElement = elements.getElement(CardElement);
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: cardElement,
+    });
+
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(paymentMethod);
+      // Send paymentMethod.id to your backend to complete the payment
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+  );
+}
+
+export default CheckoutForm;
