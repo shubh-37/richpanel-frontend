d042ae97825cf4fb27c677eaa1c2d6752831631b
Shubh Arya <shubh.arya37@gmail.com>
2024-08-03 06:23:08 +0530

    formatting changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import "./App.css";
-import Login from "./pages/LoginPage";
-import Register from "./pages/RegisterPage";
-import SubscriptionDetail from "./pages/SubscriptionDetailPage";
-import Checkout from "./pages/CheckoutPage";
-import Home from "./pages/Homepage";
-import RequiresAuth from "./components/RequiresAuth";
+import './App.css';
+import Login from './pages/LoginPage';
+import Register from './pages/RegisterPage';
+import SubscriptionDetail from './pages/SubscriptionDetailPage';
+import Checkout from './pages/CheckoutPage';
+import Home from './pages/Homepage';
+import RequiresAuth from './components/RequiresAuth';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/login" element={ <Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/plans" element={ <RequiresAuth><SubscriptionDetail /></RequiresAuth>} /> 
         <Route path="/checkout" element={<RequiresAuth><Checkout /></RequiresAuth>} />
