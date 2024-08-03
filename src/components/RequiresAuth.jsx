ac7ac8753fc539ebc9618937b865e5ae7ba57146
Shubh Arya <shubh.arya37@gmail.com>
2024-08-03 06:28:55 +0530

    fix minor issue

--- src/components/RequiresAuth.jsx
+++ src/components/RequiresAuth.jsx
@@ -3,5 +3,5 @@ import { Navigate } from 'react-router-dom';
 // eslint-disable-next-line react/prop-types
 export default function RequiresAuth({ children }) {
   const token = localStorage.getItem('token');
-  return token.length > 0 ? children : <Navigate to="/login" />;
+  return token ? children : <Navigate to="/login" />;
 }
